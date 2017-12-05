c72792f368b07b010048ba3834bc9a3251426fde
Anthagonas <the_rod_21@hotmail.com>
2017-12-05 14:22:15 +0100

    added binary image

--- scripts/skeletonize.js
+++ scripts/skeletonize.js
@@ -6,3 +6,37 @@
 // 3/ thinning (removing pixels around non-edge pixels)
 //needed : edge detection, selecting pixels to be removed (must not select end-of-line pixels).
 //			thinning method, based on the hit and miss algorithm?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Display uint8 images
+ */
+let img0 = new T.Image('uint8',360,288);
+img0.setPixels(boats_pixels);
+let win0 = new T.Window('Boats');
+let view0 = T.view(img0.getRaster());
+// Create the window content from the view
+win0.addView(view0);
+// Add the window to the DOM and display it
+win0.addToDOM('workspace');
