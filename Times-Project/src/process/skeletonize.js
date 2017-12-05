f4d228afded4925722052282b6c63593f2538d99
Anthagonas <the_rod_21@hotmail.com>
2017-12-05 14:49:20 +0100

    fixed image loading
    fixe the image loading on firefox for the skeletonize tests

--- Times-Project/src/process/skeletonize.js
+++ Times-Project/src/process/skeletonize.js
@@ -32,9 +32,9 @@
 /**
  * Display uint8 images
  */
-let img0 = new T.Image('uint8',360,288);
-img0.setPixels(boats_pixels);
-let win0 = new T.Window('Boats');
+let img0 = new T.Image('uint8',95,25);
+img0.setPixels(b_image);
+let win0 = new T.Window('Skeletonize');
 let view0 = T.view(img0.getRaster());
 // Create the window content from the view
 win0.addView(view0);
