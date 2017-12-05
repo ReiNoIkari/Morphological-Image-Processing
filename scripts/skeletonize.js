88861bc52479f888fe0cfa8c36249d69698c3b4d
Anthagonas <the_rod_21@hotmail.com>
2017-12-05 08:40:18 +0100

    added script files
    started skeletonize and watershed files, no code implemented yet

--- scripts/skeletonize.js
+++ scripts/skeletonize.js
@@ -1,1 +1,8 @@
-
+//Skeletonize
+//
+//algorithm (to be enhanced) :
+// 1/ edge detection
+// 2/ pixels to be removed ? (true if existing non-edge pixels)
+// 3/ thinning (removing pixels around non-edge pixels)
+//needed : edge detection, selecting pixels to be removed (must not select end-of-line pixels).
+//			thinning method, based on the hit and miss algorithm?
