c31e5388874e03d81f49aa03b77f6eb59ae57b01
Anthagonas <the_rod_21@hotmail.com>
2017-12-05 15:00:01 +0100

    fixed b_image2

--- Times-Project/src/process/skeletonize.js
+++ Times-Project/src/process/skeletonize.js
@@ -32,8 +32,8 @@
 /**
  * Display uint8 images
  */
-let img0 = new T.Image('uint8',95,25);
-img0.setPixels(b_image);
+let img0 = new T.Image('uint8',500,500);
+img0.setPixels(b_image2);
 let win0 = new T.Window('Skeletonize');
 let view0 = T.view(img0.getRaster());
 // Create the window content from the view
