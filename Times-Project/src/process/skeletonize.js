fe3094be94c7fe02bd90c249865ea86c02ef4f4e
Anthagonas <the_rod_21@hotmail.com>
2017-12-07 13:46:59 +0100

    removed comments

--- Times-Project/src/process/skeletonize.js
+++ Times-Project/src/process/skeletonize.js
@@ -92,13 +92,4 @@ win1.addView(view1);
 // Add the window to the DOM and display it
 win1.addToDOM('workspace');
 
-//TODO : Figure why original image doesn't display normally
-/*//test original image
-let img2 = new T.Image('uint8',500,500);
-img2.setRaster(img0.getRaster());
-let win2 = new T.Window('test');
-let view2 = T.view(img2.getRaster());
-// Create the window content from the view
-win1.addView(view2);
-// Add the window to the DOM and display it
-win1.addToDOM('workspace');*/
+//New display code? examples/adaptativeTest.js
