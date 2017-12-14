6722bba3b0bab80144b35d3f05175d930b0e25bd
Adrien MS <aeringw@hotmail.fr>
2017-12-14 12:52:13 +0100

    update

--- Times-Project/src/process/dilate.js
+++ Times-Project/src/process/dilate.js
@@ -29,26 +29,30 @@
  *
  * @param {type} <name> - <Description>
  * @return {type} - <Description>
- *
+ 
  * @author TODO
  */
 
 const dilate = function(img,copy=true,struct=[0,1,0,1,1,1,0,1,0]){
+
+
   let output = img;
   let r_output = output.getRaster();
   let struct_Center=(struct.length+1)/2; 
   let value_center_pixel=0;
-  let img_pixel_list=[];
+  let img_pixel_list=[[]];
   var outside = Boolean(false);
+
+
   //Loop to store pixels value of the whole image, easier to check for neighbours later
   for (let j =0; j<r_output.height; j++){
     for(let i=0;i<r_output.width; i++){
       img_pixel_list.push(r_output.getPixel(j,i));
     }
   }
-  for(let k=0; k<img_pixel_list.length; k++) { //parcours la liste de pixels de l'image en x
-    for(let l=0; l=img_pixel_list[k].length; l++) {//parcours la liste de pixels de l'image en y
-      if (img_pixel_list[k]==img_pixel_list[k][l]==struct_Center) //cherche dans la liste de pixels valeur du centre de l'élement structurant
+  for(let k=0; k<img_pixel_list.height; k++) { //parcours la liste de pixels de l'image en x
+    for(let l=0; l=img_pixel_list.width; l++) {//parcours la liste de pixels de l'image en y
+      if (img_pixel_list[k]==img_pixel_list[k][l]==struct_Center){ //cherche dans la liste de pixels valeur du centre de l'élement structurant
         for(let m = -(struct.length-struct_Center);m<struct.length-struct_Center;m++){// On cherche dans la liste de pixel (en x) dans une zone de la taille elmt struct 
           for(let n = -(struct.length-struct_Center);n<struct.length-struct_Center;n++){// On cherche dans la liste de pixel (en y) dans une zone de la taille elmt struct
             if (k + m < 0 || k + m > r_output.width - 1) {//check si on est à l'exterieur de l'image en x ou pas, si oui outside=true
@@ -62,17 +66,18 @@ const dilate = function(img,copy=true,struct=[0,1,0,1,1,1,0,1,0]){
             //getpixels sur l'iamge
             //getpixels sur elmt
             //si dif prendre valeur de elmt
-
-          }
-            
+              
+            }
+          } 
         }
-   
-
+      }
     }
   }
-
+  console.log(img_pixel_list);
   return output;
 }
+
+
 //1st window
 let img0 = new T.Image('uint8',500,500);
 img0.setPixels(b_image2);
@@ -84,6 +89,7 @@ win0.addView(view0);
 win0.addToDOM('workspace');
 
 //2nd window
+
 let img1 = dilate(img0);
 let win1 = new T.Window('Dilated');
 let view1 = T.view(img1.getRaster());
