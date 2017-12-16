044e9eaac2a88d466f7ecbdbf980e44239dc6c54
Adrien MS <aeringw@hotmail.fr>
2017-12-16 17:42:35 +0100

    Multiple s minor bug corrected, but still wip

--- Times-Project/src/process/dilate.js
+++ Times-Project/src/process/dilate.js
@@ -45,10 +45,10 @@ const worskpace = function(x,y,r_output,r_struct) {
   //i et j start 1 car radius minimum
   //peut etre redondance?? 
   for (let i=1;i<=radius_struct_x;i++){
-    for (let j=1:j<=radius_struct_y;j++){
+    for (let j=1;j<=radius_struct_y;j++){
       //k et l permettend de limité la zone de travail en partant d'un pixel donnée de -radius à +radius
-      for(k;k>=-radius_struct_y && <=radius_struct_y;k++) {
-        for (l;l>=-radius_struct_x && <=radius_struct_x;l++){
+      for(k;k >=-radius_struct_y && k<= radius_struct_y;k++){
+        for (l;l >=-radius_struct_x && l<= radius_struct_x;l++){
           //conditions pour les voisins autour d'un pixels donnée tourne de en haut à gauche sens aiguille montre
           //Cherhe dans la zone taille struct element les 0 dans raster image. Si 0 mais 1 dans raster struc change valeur par 2 pour eviter
           //résultat de dilation n'est lieu
@@ -58,7 +58,7 @@ const worskpace = function(x,y,r_output,r_struct) {
           // }
           
           if (r_output.getPixel(x-i,y-j)==0 && struct_raster.getPixel(x-i,y-j)==1){
-            r_output.setPixels(x,y,2));
+            r_output.setPixels(x,y,2);
           }
           if (r_output.getPixel(x-i,y)==0 && struct_raster.getPixel(x-i,y)==1){
             r_output.setPixels(x,y,2);
@@ -95,41 +95,25 @@ const process = function(r_output){
 				r_output.setPixel(x,y,1);
 			}
 		}
-	};
-const dilate = function(img,copy=true,struct=[0,1,0,1,1,1,0,1,0]){
+  }
+};
+const dilate = function(img,struct,copy=true){
 
   let output = img;
   let r_output = output.getRaster();
-  let r_struct=struct.getRaster();
+  let r_struct=  struct.getRaster();
   let struct_Center=(struct.length+1)/2; 
-  let value_struc_center=struct.xy(struct_Center);
   let radius_y = struct.height-struct_Center
   let radius_x = struct.width-struct_Center
+  let value_struc_center=struct.xy(struct_Center);
   let value_center_pixel=0;//a chnger valeur fausse pour l'instant
 
   var outside = Boolean(false);
 
-
-   for(let k=0; k<r_output.height; k++) { //parcours la liste de pixels de l'image en x
+  for(let k=0; k<r_output.height; k++) { //parcours la liste de pixels de l'image en x
     for(let l=0; l=r_output.width; l++) {//parcours la liste de pixels de l'image en y
       if (r_output.getPixel(k,l)==struct_Center){ //cherche dans la liste de pixels valeur du centre de l'élement structurant
-        for(let m = -radius_x;m<radius_x;m++){// On cherche dans la liste de pixel (en x) dans une zone de la taille elmt struct 
-          for(let n = -radius_y;n<radius_y;n++){// On cherche dans la liste de pixel (en y) dans une zone de la taille elmt struct
-            if (k + m < 0 || k + m > r_output.width - 1) {//check si on est à l'exterieur de l'image en x ou pas, si oui outside=true
-              outside = true;
-            }
-            if (l + n < 0 || l + n > r_output.height - 1) {//check si on est à l'exterieur de l'image en y ou pas, si oui outside=true
-              outside = true;
-            }
-            if (!outside) {//si on est à l'interieur de l'image :
-            //todo : 
-            //getpixels sur l'iamge
-            //getpixels sur elmt
-            //si dif prendre valeur de elmt
-              
-            }
-          } 
-        }
+        worskpace(k,l,r_output,r_struct);      
       }
     }
   }
@@ -139,7 +123,8 @@ const dilate = function(img,copy=true,struct=[0,1,0,1,1,1,0,1,0]){
 }
 
 
-//1st window
+//1st window :original 
+
 let img0 = new T.Image('uint8',500,500);
 img0.setPixels(b_image2);
 let win0 = new T.Window('Original');
@@ -149,9 +134,21 @@ win0.addView(view0);
 // Add the window to the DOM and display it
 win0.addToDOM('workspace');
 
-//2nd window
 
-let img1 = dilate(img0);
+//2nd windows : struc element
+
+let img3 = new T.Image('uint8',200,200);
+img3.setPixels(struc_cross);
+let win3 = new T.Window('Structuring element');
+let view3 = T.view(img3.getRaster());
+// Create the window content from the view
+win0.addView(view3);
+// Add the window to the DOM and display it
+win0.addToDOM('workspace');
+
+//3nd window :result 
+
+let img1 = dilate(img0,img3);
 let win1 = new T.Window('Dilated');
 let view1 = T.view(img1.getRaster());
 // Create the window content from the view
