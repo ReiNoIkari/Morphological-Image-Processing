4221e06c1253675850c89f240ef14b277b0b8d63
Adrien MS <aeringw@hotmail.fr>
2017-12-16 21:54:59 +0100

    Format style more like Antha

--- Times-Project/src/process/dilate.js
+++ Times-Project/src/process/dilate.js
@@ -33,7 +33,7 @@
  * @author TODO
  */
 
-const process_operation = function(r_output){
+const process_operation = function(r_output,copy=true){
   for(let i=0;i<=r_output.height;i++) {
 		for (let j=0;j<=r_output.width;j++){
 			if (r_output.getPixel(i,j)==2) {
@@ -44,61 +44,85 @@ const process_operation = function(r_output){
   return r_output;
 };
 
-const dilate = function(img,struct,copy=true){
-
-  let temp = new T.Image('uint8',img.width,img.height);
-	temp.setRaster(T.Raster.from(img.getRaster(),copy));
-  // let output = img;
-  // let r_output = output.getRaster();
+const dilate_process=function(rast,struct,copy=true){
+  let r_output = T.Raster.from(rast,copy);
   let r_struct=struct.getRaster();
   let struct_Center=(r_struct.length+1)/2; 
   let value_struc_center=r_struct.xy(struct_Center);
   let x_value_struc_element=value_struc_center[0];
   let y_value_struc_element=value_struc_center[1];
-  let value_center_pixel=r_struct.getPixel(x_value_struc_element,y_value_struc_element);
-  let radius_struct_y = r_struct.height-struct_Center;
-  let radius_struct_x = r_struct.width-struct_Center;
+  let value_center_pixel=r_struct.getPixel(x_value_struc_element,y_value_struc_element+1);
+
+  //valeur abérante radius_struct_y(-878)
+  let radius_struct_y = r_struct.height-value_struc_center[1];
+  let radius_struct_x = r_struct.width-value_struc_center[0];
+
+  console.log("r_output");
+  console.log(r_output);
+
+  console.log("value_struc_center is");
+  console.log(value_struc_center);
+
+  console.log("r_struct height is");
+  console.log(r_struct.height);
+
+  console.log("r_struct width is");
+  console.log(r_struct.width);
+
+  console.log("y_value_struc_element is");
+  console.log(y_value_struc_element);
+
+  console.log("radius is");
+  console.log(radius_struct_x);
 
 
- 
-  for(let y=0; y<temp.getRaster().height; y++) { //parcours le raster de l'image en x
-    for(let x=0; x<temp.getRaster().width; x++) {//parcours le raster de l'image en y
-      if (temp.getRaster().getPixel(x,y)==value_center_pixel){    
+  for(let y=0; y<r_output.height; y++) { //parcours le raster de l'image en x
+    for(let x=0; x<r_output.width; x++) {//parcours le raster de l'image en y
+      if (r_output.getPixel(x,y)==value_center_pixel){    
         for (let i=0;i<=radius_struct_x;i++){
           for (let j=0;j<=radius_struct_y;j++){
-            if (temp.getRaster().getPixel(x-i,y-j)==0 && r_struct.getPixel(x_value_struc_element-i,y_value_struc_element-j)==255){
-              temp.getRaster().setPixels(x,y,2);
+            if (r_output.getPixel(x-i,y-j)==0 && r_struct.getPixel(x_value_struc_element-i,y_value_struc_element-j)==255){
+              r_output.setPixel(x,y,2);
             }
-            if (temp.getRaster().getPixel(x-i,y)==0 && r_struct.getPixel(x_value_struc_element-i,y_value_struc_element)==255){
-              temp.getRaster().setPixels(x,y,2);
+            if (r_output.getPixel(x-i,y)==0 && r_struct.getPixel(x_value_struc_element-i,y_value_struc_element)==255){
+              r_output.setPixel(x,y,2);
             }
-            if (temp.getRaster().getPixel(x-i,y+i)==0 && r_struct.getPixel(x_value_struc_element-i,y_value_struc_element+j)==255){
-              temp.getRaster().setPixels(x,y,2);
+            if (r_output.getPixel(x-i,y+i)==0 && r_struct.getPixel(x_value_struc_element-i,y_value_struc_element+j)==255){
+              r_output.setPixel(x,y,2);
             }
-            if (temp.getRaster().getPixel(x,y-i)==0 && r_struct.getPixel(x_value_struc_element,y_value_struc_element-j)==255){
-              temp.getRaster().setPixels(x,y,2);
+            if (r_output.getPixel(x,y-i)==0 && r_struct.getPixel(x_value_struc_element,y_value_struc_element-j)==255){
+              r_output.setPixel(x,y,2);
             }
-            if (temp.getRaster().getPixel(x,y+j2)==0 && r_struct.getPixel(x_value_struc_element,y_value_struc_element+j)==255){
-              temp.getRaster().setPixels(x,y,2);
+            if (r_output.getPixel(x,y+j)==0 && r_struct.getPixel(x_value_struc_element,y_value_struc_element+j)==255){
+              r_output.setPixel(x,y,2);
             }
-            if (temp.getRaster().getPixel(x+i,y-j)==0 && r_struct.getPixel(x_value_struc_element+i,y_value_struc_element-j)==255){
-              temp.getRaster().setPixels(x,y,2);
+            if (r_output.getPixel(x+i,y-j)==0 && r_struct.getPixel(x_value_struc_element+i,y_value_struc_element-j)==255){
+              r_output.setPixel(x,y,2);
             }
-            if (temp.getRaster().getPixel(x+i,y)==0 && r_struct.getPixel(x_value_struc_element+i,y_value_struc_element)==255){
-              temp.getRaster().setPixels(x,y,2);
+            if (r_output.getPixel(x+i,y)==0 && r_struct.getPixel(x_value_struc_element+i,y_value_struc_element)==255){
+              r_output.setPixel(x,y,2);
             }
-            if (temp.getRaster().getPixel(x+i,y+j)==0 && r_struct.getPixel(x_value_struc_element+i,y_value_struc_element+j==255)){
-              temp.getRaster().setPixels(x,y,2);
+            if (r_output.getPixel(x+i,y+j)==0 && r_struct.getPixel(x_value_struc_element+i,y_value_struc_element+j==255)){
+              r_output.setPixel(x,y,2);
             }           
           }
         }
       }
     }
   }
+  return r_output;
+}
+const dilate = function(img,struct,copy=true){
 
-  let r_output = process_operation(temp.getRaster());
+  let temp = new T.Image('uint8',img.width,img.height);
+  temp.setRaster(T.Raster.from(img.getRaster(),copy));
+
+  
+  let r_output = dilate_process(temp.getRaster(),struct,copy=true);
+  
+	r_output = process_operation(r_output,true); // re processes into 0/255 ( temporary : in order to visualize, re processes "interior" pixels into grey (125))
 	temp.setRaster(r_output);
-  return temp;
+	return temp;
 };
 
 
