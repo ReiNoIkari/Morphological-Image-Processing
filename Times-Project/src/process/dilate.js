5eb2ad31d6a42d7173e50d3e39e72799830ca297
Adrien MS <aeringw@hotmail.fr>
2017-12-16 19:50:10 +0100

    Problem spoted & added comment

--- Times-Project/src/process/dilate.js
+++ Times-Project/src/process/dilate.js
@@ -33,66 +33,6 @@
  * @author TODO
  */
 
-//check all the 8 neighbors for a pixel i
-//not cute to see but w/e
-//check raisonement es ce que les coordonée x du raster_struct coorepondent à ceux image?? Petit doute...
-const worskpace = function(x,y,r_output,r_struct) {
-  
-  let struct_Center=(r_struct.length+1)/2; 
-  let radius_struct_y = r_struct.height-struct_Center
-  let radius_struct_x = r_struct.width-struct_Center
-  //i et j sont les radius en X et Y permmetant de parcours tout les voirsins d'un pixels donnée quelques soit la taille du structurement element (3*3,5*5...)
-  //i et j start 1 car radius minimum
-  //peut etre redondance?? 
-
-  //dilate marche pas probablement à cause de ces 4 for, à check!!!!
-
-  for (let i=1;i<=radius_struct_x;i++){
-    for (let j=1;j<=radius_struct_y;j++){
-      //k et l permettend de limité la zone de travail en partant d'un pixel donnée de -radius à +radius
-
-      //redondant k et l je crois bien apres reflexion
-
-      // for(k;k >=-radius_struct_y && k<= radius_struct_y;k++){
-      //   for (l;l >=-radius_struct_x && l<= radius_struct_x;l++){
-          //conditions pour les voisins autour d'un pixels donnée tourne de en haut à gauche sens aiguille montre
-          //Cherhe dans la zone taille struct element les 0 dans raster image. Si 0 mais 1 dans raster struc change valeur par 2 pour eviter
-          //résultat de dilation n'est lieu
-
-          // if (r_output.getPixel(x-i,y-j)!=struct_raster.getPixel(x-i,y-j)){
-          //   r_output.setPixels(x,y,struct_raster.getPixel(x-i,y-j));
-          // }
-      
-      if (r_output.getPixel(x-i,y-j)==0 && struct_raster.getPixel(x-i,y-j)==255){
-        r_output.setPixels(x,y,2);
-      }
-      if (r_output.getPixel(x-i,y)==0 && struct_raster.getPixel(x-i,y)==255){
-        r_output.setPixels(x,y,2);
-      }
-      if (r_output.getPixel(x-i,y+i)==0 && struct_raster.getPixel(x-i,y+j)==255){
-        r_output.setPixels(x,y,struct_raster.getPixel(x-i,y+j));
-      }
-      if (r_output.getPixel(x,y-i)==0 && struct_raster.getPixel(x,y-j)==255){
-        r_output.setPixels(x,y,2);
-      }
-
-      if (r_output.getPixel(x,y+j)==0 && struct_raster.getPixel(x,y+j)==255){
-        r_output.setPixels(x,y,2);
-      }
-      if (r_output.getPixel(x+i,y-j)==0 && struct_raster.getPixel(x+i,y-j)==255){
-        r_output.setPixels(x,y,2);
-      }
-      if (r_output.getPixel(x+i,y)==0 && struct_raster.getPixel(x+i,y)==255){
-        r_output.setPixels(x,y,2);
-      }
-      if (r_output.getPixel(x+i,y+j)==0 && struct_raster.getPixel(x+i,y+j==255)){
-        r_output.setPixels(x,y,2);
-      }
-    }
-  }
-  return r_output;
-};
-
 const process = function(r_output){
   for(let i=0;i<=rast.height;i++) {
 		for (let j=0;j<=rast.width;j++){
@@ -103,6 +43,7 @@ const process = function(r_output){
   }
   return r_output;
 };
+
 const dilate = function(img,struct,copy=true){
 
   let temp = new T.Image('uint8',img.width,img.height);
@@ -110,27 +51,55 @@ const dilate = function(img,struct,copy=true){
   temp=temp.getRaster();
   let r_struct=  struct.getRaster();
   let struct_Center=(r_struct.length+1)/2; 
-  let radius_y = struct.height-struct_Center
-  let radius_x = struct.width-struct_Center
   let value_struc_center=r_struct.xy(struct_Center);
-  //let value_struc_center=r_struct.getPixel(struct.width/2,struct.height/2)
-  console.log("heaaa");
-  console.log(value_struc_center);
   let value_center_pixel=255;//a chnger valeur fausse pour l'instant
+ 
+  let radius_struct_y = r_struct.height-struct_Center
+  let radius_struct_x = r_struct.width-struct_Center
 
 
-  for(let k=0; k<temp.height; k++) { //parcours la liste de pixels de l'image en x
-    for(let l=0; l=temp.width; l++) {//parcours la liste de pixels de l'image en y
-      if (temp.getPixel(k,l)==value_center_pixel){ //cherche dans la liste de pixels valeur du centre de l'élement structurant
-        let r_output=worskpace(k,l,temp,r_struct);      
+  for(let y=0; y<temp.height; y++) { //parcours le raster de l'image en x
+    for(let x=0; x=temp.width; x++) {//parcours le raster de l'image en y
+      if (temp.getPixel(x,y)==value_center_pixel){    
+        for (let i=0;i<=radius_struct_x;i++){
+          for (let j=0;j<=radius_struct_y;j++){
+
+            //marche pas pck les valeurs dans le getPixel dde r_struc sont complement fausse !
+            //need more reflexion
+            if (temp.getPixel(x-i,y-j)==0 && r_struct.getPixel(x-i,y-j)==255){
+              temp.setPixels(x,y,2);
+            }
+            if (temp.getPixel(x-i,y)==0 && r_struct.getPixel(x-i,y)==255){
+              temp.setPixels(x,y,2);
+            }
+            if (temp.getPixel(x-i,y+i)==0 && r_struct.getPixel(x-i,y+j)==255){
+              temp.setPixels(x,y,r_struct.getPixel(x-i,y+j));
+            }
+            if (temp.getPixel(x,y-i)==0 && r_struct.getPixel(x,y-j)==255){
+              temp.setPixels(x,y,2);
+            }
+            if (temp.getPixel(x,y+j)==0 && r_struct.getPixel(x,y+j)==255){
+              temp.setPixels(x,y,2);
+            }
+            if (temp.getPixel(x+i,y-j)==0 && r_struct.getPixel(x+i,y-j)==255){
+              temp.setPixels(x,y,2);
+            }
+            if (temp.getPixel(x+i,y)==0 && r_struct.getPixel(x+i,y)==255){
+              temp.setPixels(x,y,2);
+            }
+            if (temp.getPixel(x+i,y+j)==0 && r_struct.getPixel(x+i,y+j==255)){
+              temp.setPixels(x,y,2);
+            }           
+          }
+        }
       }
     }
   }
 
-  //need changer return pour voir si marche ou pas 
+  r_output=process(temp);
   temp.setRaster(r_output);
   return temp;
-}
+};
 
 
 //1st window :original 
