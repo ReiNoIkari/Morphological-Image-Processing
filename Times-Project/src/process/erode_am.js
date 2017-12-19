e9376d62b05662bab0f477dcce6acf983e7cfe53
Rodolphe Tworek <tworekro@gmail.com>
2017-12-19 10:06:20 +0100

    update erode

--- Times-Project/src/process/erode_am.js
+++ Times-Project/src/process/erode_am.js
@@ -38,84 +38,61 @@
  */
 
 const process_operation = function(r_output,copy=true){
-  console.log("pleaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaase");
-  console.log( r_output.getPixel(441,499));
-  for(let i=0;i<=r_output.height;i++){
-		for (let j=0;j<=r_output.width;j++){
-			if (r_output.getPixel(i,j)==2){
-        r_output.setPixel(i,j,255);
-        // console.log("interiot");
-        // console.log( r_output.getPixel(441,499));       
-      }
-      else 
-        r_output.setPixel(i,j,0);
+    for(let i=0;i<=r_output.height;i++){
+	for (let j=0;j<=r_output.width;j++){
+	    if (r_output.getPixel(i,j)==2){
+		r_output.setPixel(i,j,255);
+		// console.log("interiot");
+		// console.log( r_output.getPixel(441,499));       
+	    }
+	    else 
+		r_output.setPixel(i,j,0);
+	}
     }
-  }
-  console.log("just before process operation");
-  console.log(r_output.getPixel(441,499));
-  return r_output;
+    return r_output;
 };
 
 const erode_process=function(raster,struct,copy=true){
-  let r_output = T.Raster.from(raster,copy);
-  let r_struct=struct.getRaster();
-  let struct_Center=(r_struct.length+1)/2; 
-  let value_struc_center=r_struct.xy(struct_Center);
-  let x_value_struc_element=value_struc_center[0];
-  let y_value_struc_element=value_struc_center[1];
-  let value_center_pixel=r_struct.getPixel(x_value_struc_element,y_value_struc_element);
-  let radius_struct_y = r_struct.height-value_struc_center[1];
-  let radius_struct_x = r_struct.width-value_struc_center[0];
+    let r_output = T.Raster.from(raster,copy);
+    let r_struct=struct.getRaster();
+    let struct_Center=(r_struct.length+1)/2; 
+    let value_struc_center=r_struct.xy(struct_Center);
+    let x_value_struc_element=value_struc_center[0];
+    let y_value_struc_element=value_struc_center[1];
+    let value_center_pixel=r_struct.getPixel(x_value_struc_element,y_value_struc_element);
+    let radius_struct_y = r_struct.height-value_struc_center[1];
+    let radius_struct_x = r_struct.width-value_struc_center[0];
 
-  var values=0;
-  console.log("taille struc");
-  console.log(r_struct.length);
+    var values=0;
 
-  for(let y=0; y<raster.height; y++) { 
-    for(let x=0; x<raster.width; x++) {
-      if (raster.getPixel(x,y)==value_center_pixel){
-        for (let rx = -radius_struct_x; rx <= radius_struct_x; rx++) {
-          for (let ry = -radius_struct_y; ry <= radius_struct_y; ry++) {
-            console.log("time to go work");
-            if (raster.getPixel(x-rx,y-ry) == r_struct.getPixel(x_value_struc_element-rx,y_value_struc_element-ry)){
-              values+=1;
-              console.log("yeah we got a match");
-              console.log("valuuues  match");
-              console.log(values); 
-            }
-          } 
-        }
-        if (values==(struct.length-1)){
-          console.log("we got a perfect ")
-          r_output.setPixel(x,y,2)
-        }
-        values=0;
-        console.log("valuuues");
-        console.log(values); 
-      } 
+    for(let y=0; y<raster.height; y++) {
+	for(let x=0; x<raster.width; x++) {
+	    if (raster.getPixel(x,y)==value_center_pixel){
+		for (let rx = -radius_struct_x; rx <= radius_struct_x; rx++) {
+		    for (let ry = -radius_struct_y; ry <= radius_struct_y; ry++) {
+			if (raster.getPixel(x-rx,y-ry) == r_struct.getPixel(x_value_struc_element-rx,y_value_struc_element-ry)){
+			    values+=1;
+			}
+		    } 
+		}
+		if (values==(struct.length-1)){
+		    r_output.setPixel(x,y,2)
+		}
+	    } 
+	    values=0;
+	}
     }
-  }
-  console.log("what the value");
-  console.log( r_output.getPixel(441,499));
-  return r_output;
+    console.log(r_output.pixelData);
+    return r_output;
 }
 
 const erode = function(img,struct,copy=true){
-
-  let temp = new T.Image('uint8',img.width,img.height);
-  temp.setRaster(T.Raster.from(img.getRaster(),copy));
-  let r_output = erode_process(temp.getRaster(),struct);
-  r_output = process_operation(r_output);
-
-  console.log("After Operations main");
-  console.log( r_output.getPixel(441,499));
-
-  temp.setRaster(r_output);
-
-  console.log("THE FINAL COUNTDOWN BEFORE LAST RETURN");
-  console.log( r_output.getPixel(441,499));
-
-	return temp;
+    let temp = new T.Image('uint8',img.width,img.height);
+    temp.setRaster(T.Raster.from(img.getRaster(),copy));
+    let r_output = erode_process(temp.getRaster(),struct);
+    r_output = process_operation(r_output);
+    temp.setRaster(r_output);
+    return temp;
 };
 
 
@@ -133,12 +110,12 @@ win0.addToDOM('workspace');
 
 let img3 = new T.Image('uint8',3,3);
 img3.setPixels(mask3by3Star);
-// let win3 = new T.Window('Structuring element');
-// let view3 = T.view(img3.getRaster());
-// //Create the window content from the view
-// win3.addView(view3);
-// //Add the window to the DOM and display it
-// win3.addToDOM('workspace');
+let win3 = new T.Window('Structuring element');
+let view3 = T.view(img3.getRaster());
+//Create the window content from the view
+win3.addView(view3);
+//Add the window to the DOM and display it
+win3.addToDOM('workspace');
 
 
 // let img3 = new T.Image('uint8',45,41);
@@ -150,7 +127,7 @@ img3.setPixels(mask3by3Star);
 // //Add the window to the DOM and display it
 // win3.addToDOM('workspace');
 
-//3nd window :result 
+//3rd window :result 
 
 let img1 = erode(img0,img3);
 let win1 = new T.Window('Eroded');
