de653a9e5e02f0f16a94aa409abcbdcb2950a5c2
Rodolphe Tworek <tworekro@gmail.com>
2017-12-22 08:57:01 +0100

    benchmark ImageJ imagelist

--- Bibliography/Step #2 JS Implementation/benchmarks/benchmark_ImageJ.js
+++ Bibliography/Step #2 JS Implementation/benchmarks/benchmark_ImageJ.js
@@ -34,24 +34,24 @@ function startBenchmark(img, functionName, parameter)
 
 //Loading image and convert it to binary
 let imageList =[];
-imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_50_50.png");
-imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_150_150.png");
-imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_250_250.png");
-imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_350_350.png");
-imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_550_550.png");
-imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_750_750.png");
-imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_1050_1050.png");
-imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_1250_1250.png");
-imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_1450_1450.png");
-imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_1600_1600.png");
+imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_50_50.png"));
+imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_150_150.png"));
+imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_250_250.png"));
+imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_350_350.png"));
+imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_550_550.png"));
+imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_750_750.png"));
+imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_1050_1050.png"));
+imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_1250_1250.png"));
+imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_1450_1450.png"));
+imageList.push(IJ.openImage("https://raw.githubusercontent.com/ReiNoIkari/Morphological-Image-Processing/master/Bibliography/Step%20%232%20JS%20Implementation/benchmarks/Images/b_1600_1600.png"));
 Prefs.blackBackground = true;
 
 //list of our IJ functions/plugins to use, and the corresponding options (obtained via IJ macro recorder)
-let functionList= ["Erode","Dilate","Open","Close-"]
+let functionList= ["Erode","Dilate","Open","Close-"];
 //let functionList= ["Skeletonize","watershed"];
-let parameterList = ["","","",""]
+let parameterList = ["","","",""];
 //let parameterList = ["",""];
-
+IJ.log("starting...\n");
 for (let i=0;i<functionList.length;i++)
 {
   for (let j=0;i<imageList.length;j++){
