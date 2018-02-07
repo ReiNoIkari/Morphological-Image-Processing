9eb7e3e5754104aec324cfce2fab9e8be4d91651
Anthagonas <tworekro@gmail.com>
2018-02-07 19:33:31 +0100

    IT FINALLY WORKS ! (idk why we added the width-1 but it is fixed now)

--- Times-Project/src/cpu/process/skeletonize.js
+++ Times-Project/src/cpu/process/skeletonize.js
@@ -4,7 +4,7 @@
 
 // 2012/09/16: 3,0 1->0
 // 2012/09/16: 24,0 2->0
-const table  =
+const table =
 	//0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1
 	 [0,0,0,0,0,0,1,3,0,0,3,1,1,0,1,3,0,0,0,0,0,0,0,0,0,0,2,0,3,0,3,3,
 	  0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,3,0,2,2,
@@ -14,10 +14,10 @@ const table  =
 	  3,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 	  2,3,1,3,0,0,1,3,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 	  2,3,0,1,0,0,0,1,0,0,0,0,0,0,0,0,3,3,0,1,0,0,0,0,2,2,0,0,2,0,0,0];
-	  
+
 // 2013/12/02: 16,6 2->0
 // 2013/12/02: 24,5 0->2
-const table2  =
+const table2 =
 	  //0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1
 	 [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,2,2,0,0,0,0,
 	  0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,2,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,
@@ -27,6 +27,7 @@ const table2  =
 	  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,
 	  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 	  0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
 
 const skeletonize = (img,copy=true) => {
 	let temp = new T.Image('uint8',img.width,img.height);
@@ -54,7 +55,7 @@ const thin = (pass, table, raster) => {
 	let bgColor = 0;
 	let pixels2 = pixels.slice(); // 2 copies de la liste des pixels (pour ne pas prendre en compte les modif en cours)
 	let v, index, code;
-    let offset, rowOffset = raster.width-1;
+    let offset, rowOffset = raster.width;
     let pixelsRemoved = 0;
 	let count = 100;
 	for (let offset=0;offset<pixels.length;offset++){
@@ -71,7 +72,7 @@ const thin = (pass, table, raster) => {
 			p8 = pixels2[offset+rowOffset];
 			p9 = pixels2[offset+rowOffset+1];
             index = 0;
-            // if (truc) a |= b c'est en gros : if (truc) {a=a} else {a=b}
+            // if (truc) a |= b -> if (truc) {a=a} else {a=b}
 			if (p1!=bgColor) index |= 1;
 			if (p2!=bgColor) index |= 2;
 			if (p3!=bgColor) index |= 4;
@@ -80,7 +81,7 @@ const thin = (pass, table, raster) => {
 			if (p8!=bgColor) index |= 32;
 			if (p7!=bgColor) index |= 64;
 			if (p4!=bgColor) index |= 128;
-			code = table[index]; // recupère le code a la position index dans la table ou table2
+			code = table[index];
 			if ((pass&1)==1) { //odd pass
 				if (code==2||code==3) {
 					v = bgColor;
