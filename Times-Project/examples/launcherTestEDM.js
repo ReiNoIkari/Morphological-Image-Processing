066a6657a84ba5bdf54fab90fc8ffcba2bd5740a
Nasthael <vtrj.dejonghe@gmail.com>
2018-02-15 22:02:05 +0100

    Ajout EDM
    Ajout de l'EDM en js et en jsfonctionnel. Il y a pour l'instant 2 launcher, 2 html, une image binaire de "Blobs" et un fichier par implémentation

---
+++ Times-Project/examples/launcherTestEDM.js
@@ -0,0 +1,19 @@
+/**
+ * Invert colors
+ *
+ * @author Jean-Christophe Taveau
+ */
+ 
+// Create an Image containing boats (from ImageJ))
+let img1 = new T.Image('uint8',256,254);
+img1.setPixels(new Uint8Array(binary_blobs_pixels));
+let raster = img1.getRaster();
+let workflow1 = cpu.pipe(EDMnonFonctionnel, cpu.view);
+let view1 = workflow1(raster);
+// Create the window content from the view
+let win1 = new T.Window(`EDM_NON_FONCTIONNEL`);
+win1.addView(view1);
+// Add the window to the DOM and display it
+win1.addToDOM('workspace');
+
+// cpu.convole(img.getRaster(),gpuEnv);
